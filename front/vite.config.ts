849ebbab078e70c9963cc39c7553f8d6defd08b1
lumiknit <aasr4r4@gmail.com>
2023-08-14 18:04:53 +0900

    .

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -24,6 +24,9 @@ export default defineConfig({
             "i18next-browser-languagedetector",
             "react-i18next",
           ],
+          icons: [
+            "react-icons",
+          ]
         },
       },
     },
