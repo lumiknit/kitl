ee1d7fc6b0d591de5dfbbc490812a50e8eb4dc53
lumiknit <aasr4r4@gmail.com>
2023-09-04 11:10:02 +0900

    .

--- front/src/dark-mode-detect.ts
+++ front/src/dark-mode-detect.ts
@@ -1,6 +1,22 @@
 // Query dark mode
 
 (() => {
+  let timeout = 100;
+  const updateThemeColor = () => {
+    const bgColor = getComputedStyle(document.documentElement).getPropertyValue(
+      "--kitl-bg-color",
+    );
+    console.log("bgColor", bgColor);
+    if (!bgColor) {
+      if (timeout > 10000) return;
+      timeout *= 2;
+      setTimeout(updateThemeColor, timeout);
+      return;
+    }
+    // Set theme color
+    const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+    if (metaThemeColor) metaThemeColor.setAttribute("content", bgColor);
+  };
   const onChangeDarkMode = (isDarkMode: boolean) => {
     if (isDarkMode) {
       console.log("Dark mode");
@@ -10,17 +26,11 @@
       console.log("Light mode");
       document.documentElement.setAttribute("data-bs-theme", "light");
     }
-    setTimeout(() => {
-      const bgColor = getComputedStyle(
-        document.documentElement,
-      ).getPropertyValue("--kitl-bg");
-      // Set theme color
-      const metaThemeColor = document.querySelector('meta[name="theme-color"]');
-      if (metaThemeColor) metaThemeColor.setAttribute("content", bgColor);
-    });
+    setTimeout(updateThemeColor);
   };
-
   const match = window.matchMedia("(prefers-color-scheme: dark)");
   match.addEventListener("change", e => onChangeDarkMode(e.matches));
   onChangeDarkMode(match.matches);
+
+  window.addEventListener("load", updateThemeColor);
 })();
