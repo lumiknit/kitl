36d25c8252361bd35ac455bfc996d0907fba3ce4
lumiknit <aasr4r4@gmail.com>
2023-09-21 23:07:59 +0900

    .

--- front/src/hrm/Hrm.tsx
+++ front/src/hrm/Hrm.tsx
@@ -4,6 +4,8 @@ import "./Hrm.scss";
 import { Edge, Node, View } from "./data";
 import HrmPane from "./HrmPane";
 
+import { toast } from "../block/ToastContainer";
+
 export type HrmProps = {
 	nodes: Node[];
 	edges: Edge[];
@@ -29,7 +31,9 @@ const Hrm: Component<HrmProps> = props => {
 					x: 0,
 					y: 0,
 					z: 1,
-				}}>
+				}}
+				onClick={(x, y, c) => toast("Click " + c)}
+				onDoubleClick={() => toast("Double click")}>
 				<For each={props.nodes}>
 					{node => <div class="hrm-node">{node.id}</div>}
 				</For>
--- front/src/hrm/HrmPane.tsx
+++ front/src/hrm/HrmPane.tsx
@@ -8,7 +8,10 @@ import {
 } from "solid-js";
 
 import { Box2 } from "../common";
-import { s } from "../locales";
+
+const MOVE_THRESHOLD = 4;
+const DOUBLE_CLICK_TIME = 300;
+const MULTI_TAP_TIME = 100;
 
 export type HrmTransform = {
 	x: number; // x offset
@@ -19,19 +22,15 @@ export type HrmTransform = {
 const transformToStyle = (t: HrmTransform) =>
 	`translate(${t.x}px, ${t.y}px) scale(${t.z})`;
 
-export enum ButtonState {
-	Released,
-	Pressed,
-	Moved,
-}
+type ButtonState = number;
+const BSReleased = 0;
+const BSPressed = 1;
+const BSMoved = 2;
 
 type PointerID = number;
 
 type PointerState = {
 	id: PointerID;
-	// Last position
-	lx?: number;
-	ly?: number;
 	// Current position
 	x: number;
 	y: number;
@@ -39,22 +38,44 @@ type PointerState = {
 	// Click count
 	c: number;
 	// Timestamp
-	ts: number; 
+	ts: number;
 };
 
-type PointerStates = [
-	Map<PointerID, PointerState>, // Current state
-	Map<PointerID, PointerState>, // Old state
-];
+const emptyState = {
+	id: -1,
+	x: 0,
+	y: 0,
+	b: BSReleased,
+	c: 0,
+	ts: 0,
+};
 
-const takeTwoPointerIDs = (ps: Map<PointerID, PointerState>) => {
-	const keys = ps.keys();
-	let a = keys.next();
-	if(a.done) return [];
-	const ids = [a.value];
-	a = keys.next();
-	if(!a.done) ids.push(a.value);
-	return ids;
+type DelayedTap = {
+	x: number;
+	y: number;
+	c: number; // Count
+	ts: number; // Timestamp
+	to: number; // Timeout
+};
+
+type PointerStates = {
+	tc: Map<PointerID, PointerID>; // Touches
+	c: Map<PointerID, PointerState>; // Current
+	o: Map<PointerID, PointerState>; // Old
+	dt?: DelayedTap; // Delayed tap
+	cf?: number; // Clean Functino
+};
+
+const mapTouch = (s: PointerStates, id: PointerID): PointerID => {
+	const v = s.tc.get(id);
+	if (v !== undefined) return v;
+	// From 0 find smallest unused number
+	const ids = new Set(s.tc.values());
+	for(let i = 0;; i++)
+		if(!ids.has(i)) {
+			s.tc.set(id, i);
+			return i;
+		}
 };
 
 const distSquare = (dx: number, dy: number) => dx * dx + dy * dy;
@@ -63,197 +84,214 @@ type HrmPaneProps = {
 	children: JSXElement;
 	t: HrmTransform;
 	u?: Box2<Accessor<HrmTransform>, Setter<HrmTransform>>;
+
+	onClick?: (x: number, y: number, count: number) => void;
+	onDoubleClick?: (x: number, y: number) => void;
 };
 
 const HrmPane: Component<HrmPaneProps> = props => {
-	let paneRef: HTMLDivElement | undefined = undefined;
-	let viewRef: HTMLDivElement | undefined = undefined;
+	let paneRef: HTMLDivElement | undefined, viewRef: HTMLDivElement | undefined;
 	const [t, setT] = createSignal<HrmTransform>(props.t);
-	const [pss, setPss] = createSignal<PointerStates>([
-		new Map(),
-		new Map(),
-	], {
-		"equals": false,
-	});
+	const [pss, setPss] = createSignal<PointerStates>(
+		{
+			tc: new Map(),
+			c: new Map(),
+			o: new Map(),
+		},
+		{
+			equals: false,
+		},
+	);
 
 	if (props.u) {
 		props.u[0] = t;
 		props.u[1] = setT;
 	}
 
-	const activatePointer = (
-		id: number,
-		x: number,
-		y: number,
-	) => {
+	const activatePointer = (id: number, x: number, y: number) => {
 		// Button Down / Touch Start event
 		// Try to get old state
 		const oPs = pss();
-		let p: PointerState | undefined = oPs[1].get(id);
-		if(!p) {
-			p = {
-				id: id,
-				x: 0,
-				y: 0,
-				b: ButtonState.Pressed,
-				ts: 0,
-				c: 0,
-			};
-			oPs[0].set(id, p);
-		} else {
-			oPs[1].delete(id);
-		}
-		// Update position
-		p.x = x;
-		p.y = y;
-		p.lx = undefined;
-		p.ly = undefined;
+		const p: PointerState | undefined = oPs.o.get(id) ?? emptyState;
+		oPs.o.delete(id);
 		const now = Date.now();
-		p.c = now - p.ts > 1000 ? 1 : p.c + 1;
-		p.ts = now;
-		console.log(oPs);
+		oPs.c.set(id, {
+			...p,
+			id,
+			x,
+			y,
+			b: BSPressed,
+			c: p.c === 1 && now - p.ts <= DOUBLE_CLICK_TIME ? 2 : 1,
+			ts: now,
+		});
 		setPss(oPs);
 	};
 
-	const updatePointer = (
-		id: number,
-		x: number,
-		y: number,
-	) => {
+	const updatePointer = (id: number, x: number, y: number) => {
 		// Move event
 		const oPs = pss();
-		const p = oPs[0].get(id);
-		if(!p) return false;
+		const p = oPs.c.get(id);
+		if (!p) return false;
 		// Update pointers
-		const dx = x - p.x;
-		const dy = y - p.y;
-		console.log(x, p.x, y, p.y);
-		if(p.b !== ButtonState.Moved && distSquare(dx, dy) < 4) {
+		const dx = x - p.x, dy = y - p.y;
+		if (p.b !== BSMoved && distSquare(dx, dy) < MOVE_THRESHOLD) {
 			return false;
 		}
 		// Update view
-		const firstTwo = takeTwoPointerIDs(oPs[0]);
-		switch(firstTwo.length) {
-			case 1: {
-				const f = firstTwo[0];
-				if(f === id) {
-					setT((s) => ({
-						...s,
-						x: s.x + dx,
-						y: s.y + dy,
-					}));
-				}
-			} break;
-			case 2: {
-				const fixed = oPs[0].get(firstTwo[firstTwo[0] === id ? 1 : 0]);
-				if(!fixed) break;
+		const firstTwo = [...oPs.c.keys()].slice(0, 2);
+		const l = firstTwo.length;
+		if(l === 1 && firstTwo[0] === id) {
+			setT(s => ({
+				...s,
+				x: s.x + dx,
+				y: s.y + dy,
+			}));
+		} 
+		if(l === 2) {
+			const fixed = oPs.c.get(
+				firstTwo[firstTwo[0] === id ? 1 : 0],
+			);
+			if (fixed) {
 				const newZ = Math.sqrt(
 					distSquare(fixed.x - x, fixed.y - y) /
-					distSquare(fixed.x - p.x, fixed.y - p.y));
-				setT((s) => ({
-					x: s.x + (x - p.x) / 2,
-					y: s.y + (y - p.y) / 2,
+						distSquare(fixed.x - p.x, fixed.y - p.y),
+				);
+				setT(s => ({
+					x: s.x + dx / 2,
+					y: s.y + dy / 2,
 					z: s.z * newZ,
 				}));
-			} break;
+			}
 		}
-		p.b = ButtonState.Moved;
+		p.b = BSMoved;
 		p.x = x;
 		p.y = y;
 		return true;
 	};
 
-	const deactivatePointer = (
-		id: number,
-	) => {
+	const deactivatePointer = (id: number) => {
+		const ct = window.clearTimeout, st = window.setTimeout;
 		// Button Up / Touch End event
 		const oPs = pss();
-		const p = oPs[0].get(id);
-		oPs[0].delete(id);
+		const p = oPs.c.get(id);
+		oPs.c.delete(id);
+		setPss(oPs);
+		if (!p) return;
+		oPs.o.set(id, p);
+		const oldB = p.b;
+		p.b = BSReleased;
+		if (oldB === BSMoved) {
+			p.c = 0;
+			return;
+		}
+		if (p.c === 2) {
+			props.onDoubleClick?.(p.x, p.y);
+		} else if (oPs.dt || oPs.c.size > 0) {
+			let cnt = 1;
+			if (oPs.dt) {
+				ct(oPs.dt.to);
+				cnt = oPs.dt.c + 1;
+			}
+			oPs.dt = {
+				x: p.x,
+				y: p.y,
+				c: cnt,
+				ts: Date.now(),
+				to: st(() => {
+					const oPs = pss();
+					const p = oPs.dt;
+					if (!p) return;
+					oPs.dt = undefined;
+					props.onClick?.(p.x, p.y, p.c);
+				}, MULTI_TAP_TIME),
+			};
+		} else {
+			props.onClick?.(p.x, p.y, 1);
+		}
+		if (oPs.cf) ct(oPs.cf);
+		oPs.cf = st(() => {
+			const oPs = pss();
+			oPs.o.clear();
+			oPs.cf = undefined;
+		}, DOUBLE_CLICK_TIME);
 	};
 
-	const handleMouseDown = (e: MouseEvent) => {
-		if (e.target !== paneRef) return;
-		if (e.button !== 0) return;
-		activatePointer(-1, e.screenX, e.screenY);
+	const cancelPointer = (id: number) => {
+		// Cancel event
+		if(pss().c.has(id)) {
+			pss().c.delete(id);
+			setPss(pss());
+		}
+	};
+
+	const mapTouchEvent = (f: (id: number, x: number, y: number) => void, del: Boolean = false) => (e: TouchEvent) => {
+		if(e.target !== paneRef) return;
+		for (let touch of e.changedTouches) {
+			f(mapTouch(pss(), touch.identifier), touch.clientX, touch.clientY);
+			if(del) pss().tc.delete(touch.identifier);
+		}
 		e.preventDefault();
 	};
-	const handleMouseMove = (e: MouseEvent) => {
-		if (e.target !== paneRef) return;
-		if ((e.buttons & 1) === 0) return;
-		if (updatePointer(-1, e.screenX, e.screenY)) {
+
+	const eventHandlers: any = {
+		mousedown: (e: MouseEvent) => {
+			if (e.target === paneRef && e.button === 0) {
+				activatePointer(-1, e.screenX, e.screenY);
+				e.preventDefault();
+			}
+		},
+		mousemove: (e: MouseEvent) => {
+			if ((e.buttons & 1) !== 0 && updatePointer(-1, e.screenX, e.screenY)) {
+				e.preventDefault();
+			}
+		},
+		mouseup: (e: MouseEvent) => {
+			if (e.button === 0) {
+				deactivatePointer(-1);
+			}
+		},
+		mouseleave: (e: MouseEvent) => {
+			if (e.target === paneRef) {
+				cancelPointer(-1);
+			}
+		},
+		touchstart: mapTouchEvent(activatePointer),
+		touchmove: mapTouchEvent(updatePointer),
+		touchend: mapTouchEvent(deactivatePointer, true),
+		touchcancel: mapTouchEvent(cancelPointer, true),
+		wheel: (e: WheelEvent) => {
 			e.preventDefault();
-		}
-	};
-	const handleMouseUp = (e: MouseEvent) => {
-		if (e.button !== 0) return;
-		deactivatePointer(-1);
-	};
-	const handleMouseLeave = (e: MouseEvent) => {
-		if (e.target !== paneRef) return;
-		deactivatePointer(-1);
-	};
-	const handleTouchStart = (e: TouchEvent) => {
-		if (e.target !== paneRef) return;
-		for (let touch of e.changedTouches) {
-			activatePointer(touch.identifier, touch.clientX, touch.clientY);
-		}
-		e.preventDefault();
-	};
-	const handleTouchMove = (e: TouchEvent) => {
-		e.preventDefault();
-		for (let touch of e.changedTouches) {
-			updatePointer(touch.identifier, touch.clientX, touch.clientY);
-		}
-	};
-	const handleTouchEnd = (e: TouchEvent) => {
-		for (let touch of e.changedTouches) {
-			deactivatePointer(touch.identifier);
-		}
-		e.preventDefault();
-	};
-	const handleWheel = (e: WheelEvent) => {
-		e.preventDefault();
-		const x = e.clientX;
-		const y = e.clientY;
-		const dz = Math.pow(2, -e.deltaY / 100);
-		setT(s => ({
-			x: s.x + (x - s.x) * (1 - dz),
-			y: s.y + (y - s.y) * (1 - dz),
-			z: s.z * dz,
-		}));
+			const dz = Math.pow(2, -e.deltaY / 100);
+			setT(s => ({
+				x: s.x + (e.clientX - s.x) * (1 - dz),
+				y: s.y + (e.clientY - s.y) * (1 - dz),
+				z: s.z * dz,
+			}));
+		},
 	};
 
 	createEffect(() => {
-		paneRef?.addEventListener("wheel", handleWheel, { passive: false });
-		paneRef?.addEventListener("touchstart", handleTouchStart, { passive: false });
-		paneRef?.addEventListener("touchmove", handleTouchMove, { passive: false });
-		paneRef?.addEventListener("touchend", handleTouchEnd, { passive: false });
-		paneRef?.addEventListener("touchcancel", handleTouchEnd, { passive: false });
-		return () => {
-			paneRef?.removeEventListener("wheel", handleWheel);
-			paneRef?.removeEventListener("touchstart", handleTouchStart);
-			paneRef?.removeEventListener("touchmove", handleTouchMove);
-			paneRef?.removeEventListener("touchend", handleTouchEnd);
-			paneRef?.removeEventListener("touchcancel", handleTouchEnd);
-		};
+		if (paneRef) {
+			for (let k in eventHandlers) {
+				paneRef.addEventListener(k, eventHandlers[k]);
+			}
+			return () => {
+				if (paneRef)
+					for (let k in eventHandlers) {
+						paneRef.removeEventListener(k, eventHandlers[k]);
+					}
+			};
+		}
 	});
 
 	return (
-		<div
-			ref={paneRef}
-			class="hrm-pane"
-			onMouseDown={handleMouseDown}
-			onMouseMove={handleMouseMove}
-			onMouseUp={handleMouseUp}
-			onMouseLeave={handleMouseLeave}>
+		<div ref={paneRef} class="hrm-pane">
 			<div
 				class="hrm-view"
 				ref={viewRef}
 				style={{
-					transform: transformToStyle(t())
-				}}
-			>
+					transform: transformToStyle(t()),
+				}}>
 				{props.children}
 			</div>
 		</div>
